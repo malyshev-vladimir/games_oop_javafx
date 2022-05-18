b5201a9555d461cc8476063bd587b91c6c248285
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-05-18 23:51:06 +0200

    2. Каркас шахматной доски [#792]. Tests for method way.

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -2,7 +2,6 @@ package ru.job4j.chess.firuges.black;
 
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
-import ru.job4j.chess.firuges.Figure;
 import static org.junit.Assert.*;
 
 public class BishopBlackTest {
@@ -16,9 +15,17 @@ public class BishopBlackTest {
 
     @Test
     public void testCopy() {
-        Figure in = new BishopBlack(Cell.C8);
+        BishopBlack in = new BishopBlack(Cell.C8);
         Cell out = in.copy(Cell.E6).position();
         Cell expected = Cell.E6;
         assertEquals(expected, out);
+    }
+
+    @Test
+    public void testWay() {
+        BishopBlack in = new BishopBlack(Cell.C1);
+        Cell[] out = in.way(Cell.G5);
+        Cell[] expected = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        assertArrayEquals(expected, out);
     }
 }
